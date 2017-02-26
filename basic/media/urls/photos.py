c82d972842000e22d68e398a7646d0ac8e922130
Benjamin Turner <benturn@gmail.com>
2017-02-26 16:09:33 -0500

    update media urls

--- basic/media/urls/photos.py
+++ basic/media/urls/photos.py
@@ -1,12 +1,12 @@
-from django.conf.urls import patterns, url
+from django.conf.urls import url
 from django.views.generic import DetailView, ListView
 
 from basic.media.models import Photo, PhotoSet
 
 
-urlpatterns = patterns('',
+urlpatterns = [
     url(r'^sets/(?P<slug>[-\w]+)/$', DetailView.as_view(model=PhotoSet), name='photo_set_detail'),
     url(r'^sets/$', ListView.as_view(model=PhotoSet), name='photo_set_list'),
     url(r'^(?P<slug>[-\w]+)/$', DetailView.as_view(model=Photo), name='photo_detail'),
     url(r'^$', ListView.as_view(model=Photo), name='photo_list'),
-)
+]
