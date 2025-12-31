fbb766a84d927e1b13a77f2089d05dafe37654ac
Benjamin Turner <benturn@gmail.com>
2025-12-30 16:04:06 -0800

    media updates

---
+++ basic/media/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class BasicMediaConfig(AppConfig):
+    name = "basic.media"
+    verbose_name = "Basic Media"
--- basic/media/models.py
+++ basic/media/models.py
@@ -1,13 +1,13 @@
-from django.db import models
-from django.db.models import permalink
-from django.conf import settings
-from tagging.fields import TagField
+import json
 
-import tagging
+from django.db import models
+from django.conf import settings
+from django.urls import reverse
 
 
 class AudioSet(models.Model):
     """AudioSet model"""
+
     title = models.CharField(max_length=255)
     slug = models.SlugField()
     description = models.TextField(blank=True)
@@ -21,19 +21,24 @@ class AudioSet(models.Model):
     def __unicode__(self):
         return '%s' % self.title
 
-    @permalink
     def get_absolute_url(self):
-        return ('audio_set_detail', None, { 'slug': self.slug })
+        return reverse('audio_set_detail', kwargs={'slug': self.slug})
 
 
 class Audio(models.Model):
     """Audio model"""
+
     title = models.CharField(max_length=255)
     slug = models.SlugField()
-    still = models.FileField(upload_to='audio_stills', blank=True, help_text='An image that will be used as a thumbnail.')
-    audio = models.FilePathField(path=settings.MEDIA_ROOT+'audios/', recursive=True)
+    still = models.FileField(
+        upload_to='audio_stills',
+        blank=True,
+        help_text='An image that will be used as a thumbnail.',
+    )
+    audio = models.FilePathField(
+        path=settings.MEDIA_ROOT + "audios/", recursive=True
+    )
     description = models.TextField(blank=True)
-    tags = TagField()
     uploaded = models.DateTimeField(auto_now_add=True)
     modified = models.DateTimeField(auto_now=True)
 
@@ -44,41 +49,58 @@ class Audio(models.Model):
     def __unicode__(self):
         return '%s' % self.title
 
-    @permalink
     def get_absolute_url(self):
-      return ('audio_detail', None, { 'slug': self.slug })
+        return reverse('audio_detail', kwargs={'slug': self.slug})
 
 
 class PhotoSet(models.Model):
     """PhotoSet model"""
+
     title = models.CharField(max_length=255)
     slug = models.SlugField()
     description = models.TextField(blank=True)
-    cover_photo = models.ForeignKey('Photo', on_delete=models.CASCADE, blank=True, null=True)
+    cover_photo = models.ForeignKey(
+        'Photo', on_delete=models.CASCADE, blank=True, null=True
+    )
     photos = models.ManyToManyField('Photo', related_name='photo_sets')
     created = models.DateTimeField(auto_now_add=True)
     modified = models.DateTimeField(auto_now=True)
 
     class Meta:
-      db_table = 'media_photo_sets'
+        db_table = 'media_photo_sets'
 
     def __unicode__(self):
-      return '%s' % self.title
+        return '%s' % self.title
 
-    @permalink
     def get_absolute_url(self):
-      return ('photo_set_detail', None, { 'slug': self.slug })
+        return reverse('photo_set_detail', kwargs={'slug': self.slug})
 
 
 class Photo(models.Model):
     """Photo model"""
+
     LICENSES = (
-        ('http://creativecommons.org/licenses/by/2.0/',         'CC Attribution'),
-        ('http://creativecommons.org/licenses/by-nd/2.0/',      'CC Attribution-NoDerivs'),
-        ('http://creativecommons.org/licenses/by-nc-nd/2.0/',   'CC Attribution-NonCommercial-NoDerivs'),
-        ('http://creativecommons.org/licenses/by-nc/2.0/',      'CC Attribution-NonCommercial'),
-        ('http://creativecommons.org/licenses/by-nc-sa/2.0/',   'CC Attribution-NonCommercial-ShareAlike'),
-        ('http://creativecommons.org/licenses/by-sa/2.0/',      'CC Attribution-ShareAlike'),
+        ('http://creativecommons.org/licenses/by/2.0/', 'CC Attribution'),
+        (
+            'http://creativecommons.org/licenses/by-nd/2.0/',
+            'CC Attribution-NoDerivs',
+        ),
+        (
+            'http://creativecommons.org/licenses/by-nc-nd/2.0/',
+            'CC Attribution-NonCommercial-NoDerivs',
+        ),
+        (
+            'http://creativecommons.org/licenses/by-nc/2.0/',
+            'CC Attribution-NonCommercial',
+        ),
+        (
+            'http://creativecommons.org/licenses/by-nc-sa/2.0/',
+            'CC Attribution-NonCommercial-ShareAlike',
+        ),
+        (
+            'http://creativecommons.org/licenses/by-sa/2.0/',
+            'CC Attribution-ShareAlike',
+        ),
     )
     title = models.CharField(max_length=255)
     slug = models.SlugField()
@@ -86,20 +108,19 @@ class Photo(models.Model):
     taken_by = models.CharField(max_length=100, blank=True)
     license = models.URLField(blank=True, choices=LICENSES)
     description = models.TextField(blank=True)
-    tags = TagField()
     uploaded = models.DateTimeField(auto_now_add=True)
     modified = models.DateTimeField(auto_now=True)
-    _exif = models.TextField(blank=True) 
+    _exif = models.TextField(blank=True)
 
     class Meta:
         db_table = 'media_photos'
 
     def _set_exif(self, d):
-        self._exif = simplejson.dumps(d)
+        self._exif = json.dumps(d)
 
     def _get_exif(self):
         if self._exif:
-            return simplejson.loads(self._exif)
+            return json.loads(self._exif)
         else:
             return {}
 
@@ -112,13 +133,13 @@ class Photo(models.Model):
     def url(self):
         return '{}{}'.format(settings.MEDIA_URL, self.photo)
 
-    @permalink
     def get_absolute_url(self):
-        return ('photo_detail', None, { 'slug': self.slug })
+        return reverse('photo_detail', kwargs={'slug': self.slug})
 
 
 class VideoSet(models.Model):
     """VideoSet model"""
+
     title = models.CharField(max_length=255)
     slug = models.SlugField()
     description = models.TextField(blank=True)
@@ -132,19 +153,24 @@ class VideoSet(models.Model):
     def __unicode__(self):
         return '%s' % self.title
 
-    @permalink
     def get_absolute_url(self):
-        return ('video_set_detail', None, { 'slug': self.slug })
+        return reverse('video_set_detail', kwargs={'slug': self.slug})
 
 
 class Video(models.Model):
     """Video model"""
+
     title = models.CharField(max_length=255)
     slug = models.SlugField()
-    still = models.FileField(upload_to='video_stills', blank=True, help_text='An image that will be used as a thumbnail.')
-    video = models.FilePathField(path=settings.MEDIA_ROOT+'videos/', recursive=True)
+    still = models.FileField(
+        upload_to='video_stills',
+        blank=True,
+        help_text='An image that will be used as a thumbnail.',
+    )
+    video = models.FilePathField(
+        path=settings.MEDIA_ROOT + 'videos/', recursive=True
+    )
     description = models.TextField(blank=True)
-    tags = TagField()
     uploaded = models.DateTimeField(auto_now_add=True)
     modified = models.DateTimeField(auto_now=True)
 
@@ -154,6 +180,5 @@ class Video(models.Model):
     def __unicode__(self):
         return '%s' % self.title
 
-    @permalink
     def get_absolute_url(self):
-        return ('video_detail', None, { 'slug': self.slug })
+        return reverse('video_detail', kwargs={"slug": self.slug})
